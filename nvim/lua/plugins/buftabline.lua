ada4f82443747cdede1e9f120807e3b94be155fc
Lucas Perez <lucastperez@gmail.com>
2021-11-25 00:51:36 -0300

    Substitute vim-buftabline for buftabline.nvim

---
+++ nvim/lua/plugins/buftabline.lua
@@ -0,0 +1,28 @@
+require('buftabline').setup {
+  tab_format = " #{b}#{f} ",
+  buffer_id_index = false,
+  icon_colors = true,
+  start_hidden = false,
+  auto_hide = false,
+  disable_commands = false,
+  go_to_maps = true,
+  flags = {
+    modified = "[+]",
+    not_modifiable = "[-]",
+    readonly = "[RO]",
+  },
+  hlgroups = {
+    current = "TabLineSel",
+    normal = "TabLine",
+    active = "BufTabLineActive",
+    spacing = nil,
+    modified_current = nil,
+    modified_normal = nil,
+    modified_active = nil,
+    tabpage_current = nil,
+    tabpage_normal = nil,
+  },
+  show_tabpages = true,
+  tabpage_format = " #{n} ",
+  tabpage_position = "right",
+}
