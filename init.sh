2d2ed077d8f08536ad0458a0ec4b10c1cfeb356e
Lucas Perez <lucastperez@gmail.com>
2021-11-26 23:35:06 -0300

    Create shells directory and simplexev script/alias

--- init.sh
+++ init.sh
@@ -12,27 +12,28 @@ ln -sfni "$DIR/alacritty/" "$HOME/.config/alacritty"
 
 if [ "$INSTALL_SHELL" = z ]; then
   echo "Creating symlink to: \e[1;36m$HOME/.zshenv\e[0m"
-  ln -sfni "$DIR/zsh/zshenv" "$HOME/.zshenv"
-
-  echo "Creating symlink to: \e[1;36m$HOME/.config/zsh/.zshrc\e[0m"
-  ln -sfni "$DIR/zsh/zshrc" "$HOME/.config/zsh/.zshrc"
+  ln -sfni "$DIR/shells/zsh/zshenv" "$HOME/.zshenv"
 
   [ -d "$HOME/.config/zsh" ] || mkdir "$HOME/.config/zsh" -pv
+
+  echo "Creating symlink to: \e[1;36m$HOME/.config/zsh/.zshrc\e[0m"
+  ln -sfni "$DIR/shells/zsh/zshrc" "$HOME/.config/zsh/.zshrc"
+
   echo "Creating symlink to: \e[1;36m$HOME/.config/zsh/plugins\e[0m"
-  ln -sfni "$DIR/zsh/plugins" "$HOME/.config/zsh/plugins"
+  ln -sfni "$DIR/shells/zsh/plugins" "$HOME/.config/zsh/plugins"
 
   echo "Creating symlink to: \e[1;36m$HOME/.config/zsh/completions\e[0m"
-  ln -sfni "$DIR/zsh/completions" "$HOME/.config/zsh/completions"
+  ln -sfni "$DIR/shells/zsh/completions" "$HOME/.config/zsh/completions"
 else
   echo "Creating symlink to: \e[1;36m$HOME/.bashrc\e[0m"
-  ln -sfni "$DIR/bash/bashrc" "$HOME/.bashrc"
+  ln -sfni "$DIR/shells/bash/bashrc" "$HOME/.bashrc"
 
   echo "Creating symlink to: \e[1;36m$HOME/.bash_profile\e[0m"
-  ln -sfni "$DIR/bash/bash_profile" "$HOME/.bash_profile"
+  ln -sfni "$DIR/shells/bash/bash_profile" "$HOME/.bash_profile"
 fi
 
 echo "Creating symlink to: \e[1;36m$HOME/.aliases\e[0m"
-ln -sfni "$DIR/bash/aliases" "$HOME/.aliases"
+ln -sfni "$DIR/shells/aliases" "$HOME/.aliases"
 
 echo "Creating symlink to: \e[1;36m$HOME/.config/kitty\e[0m"
 ln -sfni "$DIR/kitty/" "$HOME/.config/kitty"
@@ -42,11 +43,12 @@ ln -sfni "$DIR/nvim/" "$HOME/.config/nvim"
 
 [ -d "$HOME/scripts" ] || mkdir "$HOME/scripts" -pv
 echo Creating symlinks to the following scripts to "\e[1;36m$HOME/scripts\e[0m" directory:
-echo Files:"\t \e[1mparse-tmux-ls.sh\e[0m, \e[1mlambda-fetch.sh\e[0m, \e[1mchmod-back-to-normal.sh\e[0m"
+echo Files:"\t \e[1mparse-tmux-ls.sh\e[0m, \e[1mlambda-fetch.sh\e[0m, \e[1mchmod-back-to-normal.sh\e[0m, \e[1msimplexev.sh\e[0m"
 echo Folders:" \e[1mgit-stuff/\e[0m, \e[1mtmux-chtsh/\e[0m"
 ln -sfni "$DIR/scripts/parse-tmux-ls.sh" "$HOME/scripts"
 ln -sfni "$DIR/scripts/lambda-fetch.sh" "$HOME/scripts"
 ln -sfni "$DIR/scripts/chmod-back-to-normal.sh" "$HOME/scripts"
+ln -sfni "$DIR/scripts/simplexev.sh" "$HOME/scripts"
 ln -sfni "$DIR/scripts/git-stuff" "$HOME/scripts"
 ln -sfni "$DIR/scripts/tmux-chtsh" "$HOME/scripts"
 
