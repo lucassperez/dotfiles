a4fd92a708ef1108377153df758f3c31a0f9cbf5
Lucas Perez <lucastperez@gmail.com>
2021-12-07 18:29:28 -0300

    Change xset to i3 config (still not the best place, probably), i3 title bars alignment (they appear in tabbed layout) and i3blocks docker script

--- i3/blocklets-scripts/docker.sh
+++ i3/blocklets-scripts/docker.sh
@@ -7,9 +7,8 @@ count=$(docker ps -q | wc -l)
 
 if [ "$BLOCK_BUTTON" = 1 -o "$BLOCK_BUTTON" = 3 ]; then
   if [ "$count" -gt 0 ]; then
-    x=$(docker ps | tail -n +2 | head -1)
-    y=$(echo "$x" | sed 's/.* \(\w*$\)/\1/')
-    notify-send -u low -i "$HOME/.config/i3/blocklets-scripts/icon-docker.png" 'Dockers' "$y"
+    running_containers=$(docker ps --format "{{.Names}} ({{.RunningFor}})\n")
+    notify-send -u low -i "$HOME/.config/i3/blocklets-scripts/icon-docker.png" 'Dockers' "$running_containers"
   else
     notify-send -u low -i "$HOME/.config/i3/blocklets-scripts/icon-docker.png" 'Dockers' 'No containers running'
   fi
