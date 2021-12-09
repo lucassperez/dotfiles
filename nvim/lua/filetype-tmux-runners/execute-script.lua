ede8ae162a7c6c6bbfbe4003a5c5222da2385357
Lucas Perez <lucastperez@gmail.com>
2021-12-08 22:27:12 -0300

    Add first line shebang verification to filetype-tmux-runners/execute-script.lua

--- nvim/lua/filetype-tmux-runners/execute-script.lua
+++ nvim/lua/filetype-tmux-runners/execute-script.lua
@@ -2,7 +2,14 @@ local filetype = vim.bo.filetype
 local full_path_filename = vim.fn.expand('%:p')
 local filename = vim.fn.expand('%')
 
-if (filetype == 'elixir') then
+local first_line = vim.fn.getline(1)
+local prog_shebang = string.match(first_line, '^#!(/[%w/ ]+)')
+
+if prog_shebang then
+  vim.fn.VtrSendCommand(prog_shebang..' '..full_path_filename)
+  print('executando '..prog_shebang..' '..filename)
+
+elseif (filetype == 'elixir') then
   vim.fn.VtrSendCommand('elixir '..full_path_filename)
   print('executando elixir '..filename)
 
@@ -11,7 +18,8 @@ elseif (filetype == 'ruby') then
   print('executando ruby '..filename)
 
 elseif (filetype == 'sh') then
-  -- TODO Verify if first line has a bash shebang to run with bash and not sh
+  -- Note that bash scripts usually have the sh filetype as well. This script
+  -- will only use bash if a shebang is specified at the first line.
   vim.fn.VtrSendCommand('sh '..full_path_filename)
   print('executando sh '..filename)
 
