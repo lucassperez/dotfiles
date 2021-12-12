ddcba082efccc14fbc6b1a169d2d4a3f6cdfff77
Lucas Perez <lucastperez@gmail.com>
2021-12-12 16:25:49 -0300

    Remove language right prompt from zsh, add i3blocks memory block clickable action and decrease i3 windows border width

--- i3/blocklets-scripts/memory.sh
+++ i3/blocklets-scripts/memory.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
+if [ "$BLOCK_BUTTON" = 1 -o "$BLOCK_BUTTON" = 3 ]; then
+  output=$(ps axh -o cmd,%mem --sort=-%mem | head -15)
+  notify-send -u low 'Maiores consumos de memória' "$output"
+fi
+
 free -h | awk '/^Mem:/ {print " "$3}'
