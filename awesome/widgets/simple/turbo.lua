31e0868a79beaeee4ff3079faca71b5ec4917569
Lucas Perez <lucastperez@gmail.com>
2021-12-18 13:26:25 -0300

    awesomewm tweaks

--- awesome/widgets/simple/turbo.lua
+++ awesome/widgets/simple/turbo.lua
@@ -10,12 +10,7 @@ local text = wibox.widget({
 local widget = wibox.widget.background()
 widget:set_widget(text)
 widget:set_fg('#00ff00')
-
-local function set_widget()
-  text:set_text('TURBO')
-end
-
-set_widget()
+text:set_text('TURBO')
 
 function widget:random_from_list(list)
   return list[math.random(#list)]
@@ -67,7 +62,9 @@ function widget:random_verb()
 end
 
 function widget:send_turbo_notification()
-  -- os.execute('dunstctl close-all')
+  if naughty.suspended then return end
+
+  naughty.destroy_all_notifications()
 
   if math.random(2) == 1 then
     phrase = widget:random_phrase()
@@ -81,7 +78,6 @@ function widget:send_turbo_notification()
     ignore_suspend = true,
     timeout = 6,
   })
-  -- os.execute('notify-send "'..phrase..'"')
 end
 
 widget:connect_signal('button::press', function(_,_,_,button)
