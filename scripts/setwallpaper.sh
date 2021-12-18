a3b439158ba33d7bad2d273b1d4621258ea7ab2f
Lucas Perez <lucastperez@gmail.com>
2021-12-17 23:11:39 -0300

    Remove Polybar from i3 and add some aliases

--- scripts/setwallpaper.sh
+++ scripts/setwallpaper.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# Debian 11 has a version with a bug actually. Better to install this version instead:
+# sudo apt-get install xwallpaper=0.7.3-1
+
 [ "$@" ] && [ -f "$@" ] && \
   xwallpaper --zoom "$@" || \
   xwallpaper --zoom "$HOME/Pictures/wallpapers/wall.jpg"
