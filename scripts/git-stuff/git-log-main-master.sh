9d915139aab7f32ec48b35d4d660df2306b22870
Lucas Perez <lucastperez@gmail.com>
2021-12-28 16:44:12 -0300

    Fix possible wrong exit code in git-log-main-master script

--- scripts/git-stuff/git-log-main-master.sh
+++ scripts/git-stuff/git-log-main-master.sh
@@ -29,7 +29,8 @@ ACTUAL_BRANCH=$(git branch --show-current)
 if [ "$ACTUAL_BRANCH" = main -o "$ACTUAL_BRANCH" = master ]; then
   echo Currently at branch "\e[91;1m$ACTUAL_BRANCH\e[0m", executing the command "\e[1mgit log -15 $@\e[0m"
   git log -15 "$@"
-  exit 0
+  # Just exit to keep the exit code of git log, be it zero or non zero
+  exit
 
 elif [ "$(git rev-parse --verify -q main)" ]; then
   BRANCH=main
