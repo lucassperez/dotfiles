69b386255c52256b1a833f6047b2ce2f0f14da79
Lucas Perez <lucastperez@gmail.com>
2022-05-19 09:54:22 -0300

    Remove debian specific AwesomeWM menu

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -23,8 +23,8 @@ local hotkeys_popup = require('awful.hotkeys_popup')
 -- require('awful.hotkeys_popup.keys')
 
 -- Load Debian menu entries
-local debian = require('debian.menu')
-local has_fdo, freedesktop = pcall(require, 'freedesktop')
+-- local debian = require('debian.menu')
+-- local has_fdo, freedesktop = pcall(require, 'freedesktop')
 
 -- {{{ Error handling
 -- Check if awesome encountered an error during startup and fell back to
@@ -110,20 +110,29 @@ myawesomemenu = {
 local menu_awesome = { 'awesome', myawesomemenu, beautiful.awesome_icon }
 local menu_terminal = { 'open terminal', terminal }
 
-if has_fdo then
-  mymainmenu = freedesktop.menu.build({
-    before = { menu_awesome },
-    after =  { menu_terminal }
-  })
-else
-  mymainmenu = awful.menu({
-    items = {
-      menu_awesome,
-      { 'Debian', debian.menu.Debian_menu.Debian },
-      menu_terminal,
-    }
-  })
-end
+mymainmenu = awful.menu({
+  items = {
+    menu_awesome,
+    menu_terminal,
+    { 'open browser', browser },
+    { 'lock screen', 'slock' },
+  }
+})
+
+-- if has_fdo then
+--   mymainmenu = freedesktop.menu.build({
+--     before = { menu_awesome },
+--     after =  { menu_terminal }
+--   })
+-- else
+--   mymainmenu = awful.menu({
+--     items = {
+--       menu_awesome,
+--       { 'Debian', debian.menu.Debian_menu.Debian },
+--       menu_terminal,
+--     }
+--   })
+-- end
 
 mylauncher = awful.widget.launcher({
   image = beautiful.awesome_icon,
@@ -243,7 +252,7 @@ awful.screen.connect_for_each_screen(
         break
       end
     end
-    -- create a special scratch tag for double buffering
+    -- Create a special scratch tag for double buffering
     s.scratch = awful.tag.add('scratch-' .. s.index, {})
     s.mytaglist = awful.widget.taglist({
        screen = s,
@@ -265,6 +274,7 @@ awful.screen.connect_for_each_screen(
         awful.button({}, 5, function () awful.layout.inc(-1) end)
       )
     )
+
     -- Create a taglist widget
     -- s.mytaglist = awful.widget.taglist({
     --   screen  = s,
