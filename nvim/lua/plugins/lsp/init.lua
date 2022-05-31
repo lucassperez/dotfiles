b943de3e50fba9555ed573c5f3dc63da7fd18ade
Lucas Perez <lucastperez@gmail.com>
2022-05-31 13:41:59 -0300

    Simple initial ruby LSP config

--- nvim/lua/plugins/lsp/init.lua
+++ nvim/lua/plugins/lsp/init.lua
@@ -4,3 +4,4 @@ require('plugins.lsp.elixir')
 require('plugins.lsp.typescript')
 -- The golang language server gopls looks for .git or go.mod file to start
 require('plugins.lsp.go')
+require('plugins.lsp.ruby')
---
+++ nvim/lua/plugins/lsp/ruby.lua
@@ -0,0 +1,25 @@
+local function on_attach(client, bufnr)
+  local function map(...)
+    vim.api.nvim_buf_set_keymap(bufnr, ...)
+  end
+  local map_opts = { noremap = true, silent = true }
+  -- I'm not sure these work with solargraph,
+  -- but at least the go to definition does work
+  map('n', [[\f]],    ':lua vim.lsp.buf.formatting()<CR>', map_opts)
+  map('n', 'K',    ':lua vim.lsp.buf.definition()<CR>', map_opts)
+  map('n', [[\k]],     ':lua vim.lsp.buf.hover()<CR>', map_opts)
+  map('n', [[\K]], ':lua vim.lsp.buf.signature_help()<CR>', map_opts)
+  map('n', [[\n]], ':lua vim.lsp.buf.rename()<CR>', map_opts) -- this is so nice
+  map('n', [[\r]], ':lua vim.lsp.buf.references()<CR>', map_opts)
+  -- map('n', [[\i]], ':lua vim.lsp.buf.implementation()<CR>', map_opts)
+  map('n', '[d', ':lua vim.diagnostic.goto_prev()<CR>', map_opts)
+  map('n', ']d', ':lua vim.diagnostic.goto_next()<CR>', map_opts)
+  map('n', [[\d]], ':lua vim.diagnostic.open_float()<CR>', map_opts)
+end
+
+require('lspconfig').solargraph.setup({
+  on_attach = on_attach,
+  flags = {
+    debounce_text_changes = 150,
+  },
+})
