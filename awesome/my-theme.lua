de90d234838dc66de112846ab4fe624de8a0effa
Lucas Perez <lucastperez@gmail.com>
2022-08-05 19:52:43 -0300

    Change home path to ~ in AwesomeWM theme

--- awesome/my-theme.lua
+++ awesome/my-theme.lua
@@ -132,7 +132,7 @@ theme.titlebar_maximized_button_normal_active = themes_path..'default/titlebar/m
 theme.titlebar_maximized_button_focus_active  = themes_path..'default/titlebar/maximized_focus_active.png'
 
 -- theme.wallpaper = themes_path..'default/background.png'
-theme.wallpaper = '/home/lucas/Pictures/wallpapers/wall.jpg'
+theme.wallpaper = '~/Pictures/wallpapers/wall.jpg'
 
 -- You can use your own layout icons like this:
 theme.layout_fairh = themes_path..'default/layouts/fairhw.png'
