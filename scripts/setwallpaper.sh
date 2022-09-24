bcdf7d943e1c014bb50bb4e43502b3deb61c0427
Lucas Perez <lucastperez@gmail.com>
2022-09-23 23:30:46 -0300

    Add xwallpaper check on setwallpaper.sh script

--- scripts/setwallpaper.sh
+++ scripts/setwallpaper.sh
@@ -3,6 +3,8 @@
 # Debian 11 has a version with a bug actually. Better to install this version instead:
 # sudo apt-get install xwallpaper=0.7.3-1
 
+which xwallpaper 2>/dev/null 1>&2 || exit
+
 [ "$@" ] && [ -f "$@" ] && \
   xwallpaper --zoom "$@" || \
   xwallpaper --zoom "$HOME/Pictures/wallpapers/wall.jpg"
