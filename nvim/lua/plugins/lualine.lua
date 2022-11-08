7eeafb33bd93367897e11cdf3578b36114e5882c
Lucas Perez <lucastperez@gmail.com>
2022-11-08 19:08:47 -0300

    Change lualine mode names

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,5 +1,3 @@
-local tema_modificado = require('plugins.modified-everforest-theme')
-
 local function getlines ()
   return [[%l/%L:%v]]
 end
@@ -8,13 +6,57 @@ local function getfile ()
   return [[%f%m%r%h%w]]
 end
 
+local function customModes ()
+  local current_mode = vim.api.nvim_get_mode().mode
+  local t = {
+    ['n']      = 'NORMAL',
+    ['no']     = 'O-PENDING',
+    ['nov']    = 'O-PENDING',
+    ['noV']    = 'O-PENDING',
+    ['no\22']  = 'O-PENDING',
+    ['niI']    = 'NORMAL',
+    ['niR']    = 'NORMAL',
+    ['niV']    = 'NORMAL',
+    ['nt']     = 'NORMAL',
+    ['ntT']    = 'NORMAL',
+    ['v']      = 'VISUAL',
+    ['vs']     = 'VISUAL',
+    ['V']      = 'V-LINE',
+    ['Vs']     = 'V-LINE',
+    ['\22']    = 'V-BLCK',
+    ['\22s']   = 'V-BLCK',
+    ['s']      = 'SELECT',
+    ['S']      = 'S-LINE',
+    ['\19']    = 'S-BLCK',
+    ['i']      = 'INSERT',
+    ['ic']     = 'INSERT',
+    ['ix']     = 'INSERT',
+    ['R']      = 'REPLCE',
+    ['Rc']     = 'REPLCE',
+    ['Rx']     = 'REPLCE',
+    ['Rv']     = 'V-REPLCE',
+    ['Rvc']    = 'V-REPLCE',
+    ['Rvx']    = 'V-REPLCE',
+    ['c']      = 'COMMND',
+    ['cv']     = 'EX',
+    ['ce']     = 'EX',
+    ['r']      = 'REPLCE',
+    ['rm']     = 'MORE',
+    ['r?']     = 'CONFIRM',
+    ['!']      = 'SHELL',
+    ['t']      = 'TERMINAL',
+  }
+
+  return t[current_mode] or current_mode
+end
+
 -- require('lualine').setup()
 require('lualine').setup {
   options = {
     -- globalstatus = true,
     icons_enabled = false,
     -- theme = 'everforest',
-    theme = tema_modificado,
+    theme = require('plugins.modified-everforest-theme'),
     -- theme = 'tokyonight',
     -- theme = 'catppuccin',
     component_separators = {'', ''},
@@ -22,7 +64,8 @@ require('lualine').setup {
     disabled_filetypes = {}
   },
   sections = { -- sections of current buffer
-    lualine_a = {'mode'},
+    -- lualine_a = {'mode'},
+    lualine_a = {customModes},
     lualine_b = {getfile},
     lualine_c = {''},
     lualine_x = {'diff'},
