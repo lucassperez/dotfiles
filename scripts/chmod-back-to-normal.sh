07b0727de18a994981c48fcd58a80326a6760919
Lucas Perez <lucastperez@gmail.com>
2022-11-13 19:56:13 -0300

    Change echo for printf in chmod script + fix force option (confirmation variable) bug

--- scripts/chmod-back-to-normal.sh
+++ scripts/chmod-back-to-normal.sh
@@ -23,28 +23,28 @@
 # --help: same as -h
 
 show_help_message() {
-  echo Usage: sh chmod-back-to-normal [OPTIONS]
-  echo
-  echo -e This script is intended to run '\e[1m'chmod -x'\e[0m' on all regular files
-  echo -e and '\e[1m'chmod 755'\e[0m' on all directories found inside current directory.
-  echo -e Note that this script '\e[1m'will remove execute permissions'\e[0m' from all regular
-  echo    files it finds, including scripts that should have those permissions.
-  echo    Symbolic links are ignored.
-  echo    This script may or may not handle very well files and directories with
-  echo    spaces on their names '):'
-  echo
-  echo   OPTIONS
-  echo
-  echo    '  -r\t\t'Executes the script recursively, going inside found directories.
-  echo -e '\t\t'This script will '\e[1m'NOT'\e[0m' go inside git directories,
-  echo    '\t\t'but will work if called from inside one.
-  echo -e '\t\t'This script does not go inside the '\e[1m'.git'\e[0m' directory
-  echo    '\t\t'itself unless called from inside it.
-  echo    '  -a\t\t'Executes the script on hidden files/directories as well.
-  echo    '\t\t'It will execute ls -a to find files and directories.
-  echo    '  -q\t\t'Does not print anything to stdout.
-  echo    '  -f\t\t'Does not ask for confirmation.
-  echo    '  -h, --help\t'Shows this help message.
+  printf 'Usage: sh chmod-back-to-normal [OPTIONS]\n'
+  printf '\n'
+  printf 'This script is intended to run \e[1mchmod -x\e[0m on all regular files\n'
+  printf 'and \e[1mchmod 755\e[0m on all directories found inside current directory.\n'
+  printf 'Note that this script \e[1mwill remove execute permissions\e[0m from all regular\n'
+  printf 'files it finds, including scripts that should have those permissions.\n'
+  printf 'Symbolic links are ignored.\n'
+  printf 'This script may or may not handle very well files and directories with\n'
+  printf 'spaces on their names ):\n'
+  printf '\n'
+  printf 'OPTIONS\n'
+  printf '\n'
+  printf '  -r\t\tExecutes the script recursively, going inside found directories.\n'
+  printf '\t\tThis script will \e[1mNOT\e[0m go inside git directories,\n'
+  printf '\t\tbut will work if called from inside one.\n'
+  printf '\t\tThis script does not go inside the \e[1m.git\e[0m directory\n'
+  printf '\t\titself unless called from inside it.\n'
+  printf '  -a\t\tExecutes the script on hidden files/directories as well.\n'
+  printf '\t\tIt will execute ls -a to find files and directories.\n'
+  printf '  -q\t\tDoes not print anything to stdout.\n'
+  printf '  -f\t\tDoes not ask for confirmation.\n'
+  printf '  -h, --help\tShows this help message.\n'
 }
 
 reexecutes_recursively() {
@@ -88,10 +88,9 @@ done
 if [ "$FORCE" != yes ] ;then
   printf 'Are you sure you want to start chmodding things? [y/N] '
   read confirmation
-fi
-
-if ! [ "$confirmation" = y -o "$confirmation" = Y -o $(echo "$confirmation" | grep -i '^yes$' ) ]; then
-  exit
+  if ! [ $confirmation = y -o "$confirmation" = Y ] || $(echo "$confirmation" | grep -iq '^yes$' ); then
+    exit
+  fi
 fi
 
 OLD_IFS="$IFS"
