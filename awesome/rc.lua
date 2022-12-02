c3ee7caec99e827552f0d8a835a299e164ec3101
Lucas Perez <lucastperez@gmail.com>
2022-12-02 09:19:07 -0300

    Add Discord to awesome menu

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -120,6 +120,7 @@ mymainmenu = awful.menu({
     { '&Terminal', terminal },
     { '&Browser', browser },
     { '&Campo Minado', 'gnome-mines' },
+    { '&Discord', 'Discord' },
     { '&Lock Screen', 'slock' },
   }
 })
