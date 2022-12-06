67f162f8df22b80f444b5fd353afc9e42712d4c8
Lucas Perez <lucastperez@gmail.com>
2022-12-05 22:55:58 -0300

    Tweak nvim settings and gitignore (title option had a bug?)
    Removed most lines that were setting an option
    that was already the default.
    Changed the wildmenu option to default as well.
    
    When using the title option, a random "nvim" would
    appear somewhere when opening a buffer. Wth?

--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -6,33 +6,17 @@ local wo = vim.wo -- window options
 local opt = vim.opt
 
 -- global
-o.hidden  = true -- podemos esconder buffers com alterações
-o.confirm = true -- mas se tentar fechar um buffer com alterações, ele primeiro pergunta se quer salvar
+o.confirm = true -- pede confirmação ao tentar fechar um buffer com alterações
 o.tabstop = 2 -- tamanho do tab
 o.shiftwidth = 2 -- empurrar as coisas por 2 espaços, somente
 o.expandtab = true -- tab com espaços
-o.title = true -- altera o título do terminal pro arquivo sendo editado
-o.titleold = 'Terminal' -- quando fechar o vim, escrever Terminal no título do terminal
-o.belloff = 'all' -- sem bips!
 o.ignorecase = true -- busca case insensitive
 o.smartcase = true -- busca case sensitive SSE tiver pelo menos uma letra maiúscula
-o.incsearch = true -- ir gerando resultados enquanto escreve a busca
-o.inccommand = 'nosplit' -- ir mostrando também no comando de substituir (ex :%s...)
-o.laststatus = 2 -- sempre mostrar a status bar
 o.scrolloff = 1 -- sempre mostra pelo menos uma linha abaixo e acima do cursor
 o.sidescrolloff = 3 -- sempre mostra pelo menos três colunas à direita e à esquerda do cursor
--- nostop or start?
-o.backspace = "indent,eol,start" -- conseguir apagar identações, também
-o.lazyredraw = true
-o.backup = false
-o.writebackup = false
+o.lazyredraw = true -- espera o macro acabar pra redesenhar a tela ao invés de ir redesenhando enquanto executa o macro
 o.mouse = 'a' -- habilita o mouse (a significa all). Sacrilégio!
 
-o.wildmenu = true
--- show everything scattered, multiple per line
--- o.wildmode = 'longest,list'
--- show on a pop up like list, one per line
-o.wildmode = 'longest:full'
 o.wildignore = '**/node_modules/**' -- porque né, ninguém merece esse treco gigante
 o.wildignorecase = true -- auto complete case insensitive
 
@@ -41,8 +25,7 @@ o.splitbelow = true
 o.splitright = true
 
 -- buffer
-bo.smartindent = true -- vai tentar o seu melhor para indentar
-bo.autoindent = true  -- as coisas automaticamente
+bo.smartindent = true -- vai tentar o seu melhor para indentar as coisas automaticamente
 bo.swapfile = false
 
 -- window
@@ -52,7 +35,8 @@ wo.relativenumber = true -- números relativos (tipo distâncias) ao cursor
 wo.numberwidth = 3 -- tamanho mínimo da coluna pros números à esquerda
 wo.wrap = false -- sem wrap quando o texto chega no final da tela
 
-opt.encoding = 'utf8'
+-- opt.encoding = 'utf8' -- eu preciso mesmo passar isso?
+
 -- List mode tem que estar ligado para conseguir usar o listchars
 wo.list = true
 opt.listchars = {
