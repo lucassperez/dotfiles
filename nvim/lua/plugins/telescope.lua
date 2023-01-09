23a3d640a9315cd25579bd2cdda36b544a886fdf
Lucas Perez <lucastperez@gmail.com>
2023-01-09 14:43:29 -0300

    [neovim] Add telescope C-p/n mappings and refactor its config a bit to use more lua

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,13 +1,16 @@
-vim.api.nvim_set_keymap('n', '<C-p>', ':lua TelescopeGitOrFindFiles()<CR>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<C-f>', ':Telescope live_grep<CR>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<leader>P', ':Telescope buffers<CR>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<leader>h', ':Telescope oldfiles<CR>', { noremap = true })
+local telescope = require('telescope')
+local telescope_builtin = require('telescope.builtin')
 
-vim.api.nvim_set_keymap('n', '<leader>zv', ':Telescope find_files cwd=/home/lucas/dotfiles/nvim/<CR>', { noremap = true })
+vim.keymap.set('n', '<C-p>', function() TelescopeGitOrFindFiles() end, { noremap = true })
+vim.keymap.set('n', '<C-f>', function() telescope_builtin.live_grep() end, { noremap = true })
+vim.keymap.set('n', '<leader>P', function() telescope_builtin.buffers() end, { noremap = true })
+vim.keymap.set('n', '<leader>h', function() telescope_builtin.oldfiles() end, { noremap = true })
+-- vim.keymap.set('n', '<leader>zv', ':Telescope find_files cwd=/home/lucas/dotfiles/nvim/<CR>', { noremap = true })
+vim.keymap.set('n', '<leader>zv', function() telescope_builtin.find_files({ cwd='/home/lucas/dotfiles/nvim/' }) end, { noremap = true })
 
--- You dont need to set any of these options. These are the default ones. Only
--- the loading is important
-require('telescope').setup({
+local actions = require('telescope.actions')
+
+telescope.setup({
   pickers = {
     lsp_references = { show_line = false },
   },
@@ -20,24 +23,29 @@ require('telescope').setup({
       i = {
         ['<C-k>'] = 'move_selection_previous',
         ['<C-j>'] = 'move_selection_next',
+
+        -- I wanted to have a "faster scrolling", but pag up/down is too fast! :sweat_smile:
+        -- ['<C-p>'] = 'results_scrolling_up',
+        -- ['<C-n>'] = 'results_scrolling_down',
+        ['<C-p>'] = actions.move_selection_previous + actions.move_selection_previous + actions.move_selection_previous,
+        ['<C-n>'] = actions.move_selection_next + actions.move_selection_next + actions.move_selection_next,
       },
     },
   },
   extensions = {
     fzf = {
-      fuzzy = true,                    -- false will only do exact matching
-      override_generic_sorter = true,  -- override the generic sorter
-      override_file_sorter = true,     -- override the file sorter
-      case_mode = 'smart_case',        -- or "ignore_case" or "respect_case"
-                                       -- the default case_mode is "smart_case"
+      fuzzy = true, -- false will only do exact matching
+      override_generic_sorter = true, -- override the generic sorter
+      override_file_sorter = true, -- override the file sorter
+      case_mode = 'smart_case', -- or "ignore_case" or "respect_case"
+      -- the default case_mode is "smart_case"
     },
   },
 })
--- To get fzf loaded and working with telescope, you need to call
--- load_extension, somewhere after setup function:
-require('telescope').load_extension('fzf')
 
-local telescope_builtin = require('telescope.builtin')
+-- To get fzf loaded and working with telescope, you need to call
+-- load_extension somewhere AFTER the setup function:
+telescope.load_extension('fzf')
 
 function TelescopeGitOrFindFiles(opts)
   local code = os.execute('git rev-parse --git-dir 2>/dev/null 1>&2')
