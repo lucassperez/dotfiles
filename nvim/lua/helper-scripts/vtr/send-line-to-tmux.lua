b540562f912c8cfd2d320b81d8ca58b641fef64a
Lucas Perez <lucastperez@gmail.com>
2023-01-14 15:50:59 -0300

    [neovim] Fix some warnings in helper-scripts/vtr/send-line-to-tmux

--- nvim/lua/helper-scripts/vtr/send-line-to-tmux.lua
+++ nvim/lua/helper-scripts/vtr/send-line-to-tmux.lua
@@ -9,12 +9,12 @@
 -- option and the regular VtrSendLinesToRunner, but it is too late now
 function sendLinesToTmux(mode)
   mode = mode or vim.fn.mode()
-  text = ''
+  local text = ''
   if mode == 'normal' or mode == 'n' then
     text = vim.fn.getline('.')
   elseif mode == 'visual' or mode == 'v' then
-    start  = tonumber(vim.fn.getpos("'<")[2])
-    finish = tonumber(vim.fn.getpos("'>")[2])
+    local start  = tonumber(vim.fn.getpos("'<")[2])
+    local finish = tonumber(vim.fn.getpos("'>")[2])
     for i = start, finish - 1 do
       text = text..vim.fn.getline(i)..'\n'
     end
