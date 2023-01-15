30a4f87b449887a40d1ec8b1775a557baa739e01
Lucas Perez <lucastperez@gmail.com>
2023-01-15 12:01:08 -0300

    [awesomewm] Refactor pavucontrol and pulsemixer shell scripts

--- awesome/widgets/simple/pavucontrol+volume-update.sh
+++ awesome/widgets/simple/pavucontrol+volume-update.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 pavucontrol "$@"
-awesome-client "require('widgets.simple.volume'):update_widget('amixer -D pulse sget Master')"
-awesome-client "require('widgets.simple.microphone'):update_widget('amixer sget Capture')"
+awesome-client "require('widgets.simple.volume'):update_widget()"
+awesome-client "require('widgets.simple.microphone'):update_widget()"
