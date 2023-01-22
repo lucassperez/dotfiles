d0da8e35fa34c4a16b767f80d9a39cdcf8b9baef
Lucas Perez <lucastperez@gmail.com>
2023-01-21 22:47:36 -0300

    [polybar] Remove some blank lines and trailing whitespaces

--- ARCHIVE/polybar/scripts/brightness-poly.sh
+++ ARCHIVE/polybar/scripts/brightness-poly.sh
@@ -5,17 +5,14 @@ if [ "$1" = increase ]; then
   delta=${2:-5}
   xbacklight -inc "$delta"
   polybar-msg hook brightness 1
-
 elif [ "$1" = decrease ]; then
   delta=${2:-5}
   xbacklight -dec "$delta"
   polybar-msg hook brightness 1
-
 elif [ "$1" = set ]; then
   value=${2:-25}
   xbacklight -set "$value"
   polybar-msg hook brightness 1
-
 elif [ "$1" = icon ]; then
   brightness=$(xbacklight -get | cut -d '.' -f 1)
   echo "%{F#aaeb6a} $brightness%"
--- ARCHIVE/polybar/scripts/dunst-poly.sh
+++ ARCHIVE/polybar/scripts/dunst-poly.sh
@@ -3,7 +3,6 @@
 if [ "$1" = toggle ]; then
   dunstctl set-paused toggle
   polybar-msg hook dunst 1
-
 elif [ "$1" = icon ]; then
   if [ "$(dunstctl is-paused)" = true ]; then
     echo "%{F#BE616E}"
