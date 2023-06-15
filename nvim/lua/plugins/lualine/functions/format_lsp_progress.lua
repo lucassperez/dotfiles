bfab0c0cb777725a3f72ee15fe95ccb3c7d91366
Lucas Perez <lucastperez@gmail.com>
2023-06-15 02:49:01 -0300

    [neovim] Use gitsigns diff in lualine, create lua/plugins/lualine/ folder and separate functions from main lualine config file into their own files inside lualine/functions/ folder

---
+++ nvim/lua/plugins/lualine/functions/format_lsp_progress.lua
@@ -0,0 +1,51 @@
+return function()
+  -- Ta fazendo sumir a tela inicial do vim! )=
+  -- A função formatLspProgress parece ser a culpada,
+  -- mais especificamente a chamada pra require('lspconfig.util')
+  -- Mais especificamente, o lspconfig.util.get_config_by_ft chama
+  -- o require('lspconfig.util'), e é esse util que tá fazendo
+  -- sumir a tela inicial do vim
+
+  -- local buf_clients = {}
+
+  -- local lspconfig_ok, lspconfig_util = pcall(require, 'lspconfig.util')
+  -- if lspconfig_ok then
+  --   -- Hopefully returns a table (list) with all the configured
+  --   -- servers for this buffer.
+  --   -- The standard get_active_clients returns empty list even
+  --   -- when that filetype had a language server configured but
+  --   -- it has stopped.
+  --   -- The reason for this is that I want to write LSP Off only
+  --   -- when there is at least one configured server for that
+  --   -- filetype, but none active, and an empty string when
+  --   -- there are no configured language servers for that filetype.
+  --   -- I hope this works!
+  --   buf_clients = lspconfig_util.get_config_by_ft(vim.bo.filetype)
+  -- else
+  --   buf_clients = vim.lsp.get_active_clients({ bufnr = 0 })
+  -- end
+
+  -- local p = require('lsp-progress').progress()
+  -- if p == '' then p = 'LSP Off' end
+  -- return p
+
+  -- Chamar a função vim.lsp.get_active_clients
+  -- também está fazendo sumir a tela inicial do vim! ):
+  -- A guarda a seguir é pra não chamá-la quando
+  -- abrimos o vim sem nenhum arquivo, ou seja,
+  -- pra manter a tela inicial.
+  if vim.bo.filetype == '' then return '' end
+
+  local buf_clients = vim.lsp.get_active_clients({ bufnr = 0 })
+
+  if #buf_clients == 0 then return '' end
+
+  local s = ''
+  for _, client in pairs(buf_clients) do
+    s = client.name .. ', ' .. s
+  end
+
+  s = s:gsub(', $', '')
+
+  return s
+end
---
+++ nvim/lua/plugins/lualine/functions/gitsigns_or_builtin_diff.lua
@@ -0,0 +1,29 @@
+-- When gitsigns is not available, I want to use the
+-- builtin lualine 'diff' component, so I return a
+-- plain string with the value 'diff', so it uses
+-- the builtin 'diff' component.
+if not pcall(require, 'gitsigns') then return 'diff' end
+
+-- Otherwise, I use a gitsigns_status_dict to build it.
+-- The reason for that is that the builtin only updates
+-- when we save the file, whereas the gitsigns one updates
+-- as we change the file, even before saving.
+return function ()
+  local t = vim.b['gitsigns_status_dict']
+  local s = ''
+  if t.added and t.added > 0 then
+    s = s .. '%#lualine_x_diff_added_command#+' .. t.added .. ' '
+  end
+  if t.changed and t.changed > 0 then
+    s = s .. '%#lualine_x_diff_modified_command#~' .. t.changed .. ' '
+  end
+  if t.removed and t.removed > 0 then
+    s = s .. '%#lualine_x_diff_removed_command#-' .. t.removed .. ' '
+  end
+
+  if s == '' then return '' end
+
+  s = s:gsub(' $', '')
+
+  return s
+end
