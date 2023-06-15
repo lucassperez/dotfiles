1319559e4d0b4bebbeaf06deac8f607d4f1028ee
Lucas Perez <lucastperez@gmail.com>
2023-06-15 18:19:10 -0300

    [neovim] Don't use space for tabs in go files

---
+++ nvim/after/ftplugin/go.lua
@@ -0,0 +1,4 @@
+-- Don't use space as tabs in golang
+vim.bo.tabstop = 2 -- tamanho do tab
+vim.bo.shiftwidth = 2 -- empurrar as coisas por 2 espaços, somente
+vim.bo.expandtab = false -- tab mesmo, e não espaços
