6470bdebd61f78daf37d7b541e868fd113e959fd
Lucas Perez <lucastperez@gmail.com>
2023-06-15 18:25:42 -0300

    [neovim] Add telescope shortcut <C-h> to which_key

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -49,6 +49,8 @@ local function setup()
       },
       mappings = {
         i = {
+          ['<C-h>'] = 'which_key',
+
           ['<C-k>'] = 'move_selection_previous',
           ['<C-j>'] = 'move_selection_next',
 
