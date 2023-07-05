7c14309b3f9911c1f0f65def48d865764b9eb78c
Lucas Perez <lucastperez@gmail.com>
2023-07-05 17:41:24 -0300

    [neovim] Fix defining lua snippets in go file

--- nvim/lua/plugins/luasnip/lua.lua
+++ nvim/lua/plugins/luasnip/lua.lua
@@ -1,12 +1,12 @@
 local luasnip = require('luasnip')
 
 local s = luasnip.snippet
--- local sn = luasnip.snippet_node
+local sn = luasnip.snippet_node
 local t = luasnip.text_node
 local i = luasnip.insert_node
 -- local f = luasnip.function_node
 local c = luasnip.choice_node
--- local d = luasnip.dynamic_node
+local d = luasnip.dynamic_node
 -- local r = luasnip.restore_node
 -- local l = require('luasnip.extras').lambda
 -- local rep = require('luasnip.extras').rep
@@ -20,48 +20,78 @@ local fmt = require('luasnip.extras.fmt').fmt
 -- local conds = require('luasnip.extras.conditions')
 -- local conds_expand = require('luasnip.extras.conditions.expand')
 
--- I made this a plain variable, but then I could
--- only use it once for some misterious reason. D:!
-local function go_error_snippet()
-  return fmt(
-    'if {} {{{}\n\t{}\n}}',
-    { i(3, 'err != nil'), i(2), c(1, {
-      fmt('{}return fmt.Errorf("{}: %w", err)', { i(2), i(1), }),
-      t('panic(err)'),
-      i(1),
-    }), })
-end
+luasnip.add_snippets('lua', {
+  s('req', d(1, function()
+    local positions = vim.fn.getpos('.')
+    local line_nr = positions[2]
+    local line_up_to_cursor = vim.fn.getline(line_nr):sub(1, positions[3])
 
-luasnip.add_snippets('go', {
-  -- if error
-  s('ier', go_error_snippet()),
-  s('ife', go_error_snippet()),
+    if line_up_to_cursor:match('^%s*$') then
+      return sn(nil, c(1, {
+        fmt('{}require(\'{}\')',
+        {
+          d(1, function (args)
+            local import_name =
+                args[1][1]
+                :gsub('.*%.(.*)$', '%1')
+                :gsub('%s*$', '') -- remove trailing whitespace
+                :gsub('^%s*', '') -- remove leading whitespace
+                :gsub('-', '_')
+                :gsub('%s', '_')
+                :lower()
 
-  -- http handler args
-  s('wr', { t('w http.ResponseWriter, r *http.Request'), }),
+            return sn(nil, fmt('local {} = ', t(import_name)))
+          end, { 2, }),
+          i(2),
+        }),
+        fmt('require(\'{}\')', { i(1), }),
+      }))
+    end
 
-  -- function + http handler args
-  s('fh', fmt('func {}(w http.ResponseWriter, r *http.Request) {{\n\t{}\n}}', { i(1), i(2), })),
+    return sn(nil, fmt('require(\'{}\')', { i(1), }))
+  end, {})),
+
+  -- if else elseif
+  s('if', fmt('if {} then\n\t{}{}{}\nend', {
+    i(1),
+    i(2),
+    c(4, {
+      t(''),
+      fmt('\n\nelseif {} then\n\t{}\n', { i(1), i(2), }),
+    }),
+    c(3, {
+      t(''),
+      fmt('\n\nelse\n\t{}\n', { i(1), }),
+    })
+  })),
 
   -- function
-  s('func',
-    fmt('func {}({}){}{{\n\t{}\n}}', {
-      c(1, { i(1), fmt('({}) {}', { i(1), i(2), }) }), -- choice for functions or methods
-      i(2), i(3, ' '), i(4),
-    })),
+  s('func', fmt('{}({})\n\t{}\nend', {
+    d(1, function()
+      -- Subtract 2 because this snippet is "2 lines tall".
+      -- The getpos('.') and getline('.') gives info about the end
+      -- of the snippet, so in this case, when we subtract 2 we get
+      -- the info about the start of the snippet.
+      local positions = vim.fn.getpos('.')
+      local line_nr = positions[2] - 2
+      local line_up_to_cursor = vim.fn.getline(line_nr):sub(1, positions[3])
 
-  -- TODO aprender a fazer isso
-  -- s('for', fmt('for {} {{\n\t{}\n}}', {
-  --   c(1, {
-  --     fmt('{}; {}; {}', {
-  --       i(1),
-  --       f(function(var_name) return (var_name[1][1]):gsub('(%w*)%s:=.*', '%1') end, { 1, }),
-  --       f(function(var_name) return (var_name[1][1]):gsub('(%w*)%s:=.*', '%1') end, { 1, })
-  --     }),
-  --     i(1),
-  --   }),
-  --   i(2),
-  -- })),
+      if line_up_to_cursor:match('[%w=]') then
+        return sn(nil, t('function '))
+      end
+
+      return sn(
+        nil,
+        c(1, {
+          fmt('local function {}', { i(1), }),
+          fmt('function {}', { i(1), }),
+        }))
+    end, {}),
+    i(2), i(3),
+  })),
+
+  -- for
+  s('for', fmt('for {} in {} do\n\t{}\nend', { i(1), i(2), i(3), })),
 })
 
 -----------------------------------
