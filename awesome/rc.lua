c8b119693ae29e1b5a11b54c1e038ac9f63b9f3b
Lucas Perez <lucastperez@gmail.com>
2023-07-07 12:58:51 -0300

    [awesomewm] Copy layoutbox widget to have a higher hover timeout ("delay_show")

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -324,7 +324,8 @@ awful.screen.connect_for_each_screen(
     s.mypromptbox = awful.widget.prompt()
     -- Create an imagebox widget which will contain an icon indicating which layout we're using.
     -- We need one layoutbox per screen.
-    s.mylayoutbox = awful.widget.layoutbox(s)
+    -- s.mylayoutbox = awful.widget.layoutbox(s)
+    s.mylayoutbox = require('widgets.my-layout-box')(s)
     s.mylayoutbox:buttons(
       gears.table.join(
         awful.button({}, 1, function() awful.layout.inc( 1) end),
