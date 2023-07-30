3fd56f93a0c8c662919c63c27d73f549b167d964
Lucas Perez <lucastperez@gmail.com>
2023-07-30 02:30:03 -0300

    [neovim] Add verbose gitsigns function

--- nvim/lua/plugins/lualine/functions/gitsigns_or_builtin_diff.lua
+++ nvim/lua/plugins/lualine/functions/gitsigns_or_builtin_diff.lua
@@ -8,18 +8,47 @@ if not pcall(require, 'gitsigns') then return 'diff' end
 -- The reason for that is that the builtin only updates
 -- when we save the file, whereas the gitsigns one updates
 -- as we change the file, even before saving.
-return function ()
+
+local verbose = true
+
+local function verboseFunc()
+  local h = require('gitsigns').get_hunks()
+  if #h == 0 then return '' end
+
+  local diff_str = ''
   local t = vim.b['gitsigns_status_dict']
-  local s = ''
   if t.added and t.added > 0 then
-    s = s .. '%#lualine_x_diff_added_command#+' .. t.added .. ' '
+    diff_str = diff_str .. '%#lualine_x_diff_added_command#+' .. t.added .. ' '
   end
   if t.changed and t.changed > 0 then
-    s = s .. '%#lualine_x_diff_modified_command#~' .. t.changed .. ' '
+    diff_str = diff_str .. '%#lualine_x_diff_modified_command#~' .. t.changed .. ' '
   end
   if t.removed and t.removed > 0 then
-    s = s .. '%#lualine_x_diff_removed_command#-' .. t.removed .. ' '
+    diff_str = diff_str .. '%#lualine_x_diff_removed_command#-' .. t.removed .. ' '
   end
+  diff_str = diff_str:gsub(' $', '')
 
-  return s:gsub(' $', '')
+  return 'hunks: ' .. #h ..  ' diff: ' .. diff_str
 end
+
+local function simplerFunc()
+  local h = require('gitsigns').get_hunks()
+  if #h == 0 then return '' end
+
+  local diff_str = ''
+  local t = vim.b['gitsigns_status_dict']
+  if t.added and t.added > 0 then
+    diff_str = diff_str .. '%#lualine_x_diff_added_command#+' .. t.added .. ' '
+  end
+  if t.changed and t.changed > 0 then
+    diff_str = diff_str .. '%#lualine_x_diff_modified_command#~' .. t.changed .. ' '
+  end
+  if t.removed and t.removed > 0 then
+    diff_str = diff_str .. '%#lualine_x_diff_removed_command#-' .. t.removed .. ' '
+  end
+  diff_str = diff_str:gsub(' $', '')
+
+  return #h .. ': ' .. diff_str
+end
+
+return verbose and verboseFunc or simplerFunc
