6004f27085c41b19ec46c4b95a843c0bae9fb91b
Lucas Perez <lucastperez@gmail.com>
2023-08-06 20:41:23 -0300

    [i3] Blindly applies stylua to ARCHIVED i3 blocklet turbo lua script

--- ARCHIVE/i3/blocklets-scripts/turbo/turbo.lua
+++ ARCHIVE/i3/blocklets-scripts/turbo/turbo.lua
@@ -18,33 +18,76 @@ function random_phrase()
     'NANO POWER CONVERTER',
     'SUBSPACE MOLECULE GENERATOR',
     'INTERDIMENSIONAL HYPERX',
-    'POWER GENETIC TRANSPORTER'
+    'POWER GENETIC TRANSPORTER',
   }
   return random_from_list(phrases)
 end
 
 function random_build_phrase()
   prefixes = {
-    'MAX', 'TURBO', 'ULTRA', 'NITRO', 'MEGA', 'BLASTER', 'HYPER', 'BIONIC',
-    'SPACE', 'EXTREME', 'ULTIMATE', 'COSMIC', 'SUPER', 'TRANSLUSCENT',
-    'FINAL', 'ROBOTIC', 'INTERGALATIC', 'NANO', 'POWER', 'SUBSPACE',
+    'MAX',
+    'TURBO',
+    'ULTRA',
+    'NITRO',
+    'MEGA',
+    'BLASTER',
+    'HYPER',
+    'BIONIC',
+    'SPACE',
+    'EXTREME',
+    'ULTIMATE',
+    'COSMIC',
+    'SUPER',
+    'TRANSLUSCENT',
+    'FINAL',
+    'ROBOTIC',
+    'INTERGALATIC',
+    'NANO',
+    'POWER',
+    'SUBSPACE',
     'INTERDIMENSIONAL',
-    'MAX', 'TURBO', 'ULTRA', 'NITRO', 'MEGA',
-    'MAX', 'TURBO', 'ULTRA', 'NITRO', 'MEGA',
+    'MAX',
+    'TURBO',
+    'ULTRA',
+    'NITRO',
+    'MEGA',
+    'MAX',
+    'TURBO',
+    'ULTRA',
+    'NITRO',
+    'MEGA',
   }
-  suffixes ={
-    'HYPERPROCESSOR', 'POWERLIGHT', 'POWER CONVERTER', 'MOLECULE GENERATOR',
-    'HYPERX', 'GENETIC TRANSPORTER', 'ACCELERATOR', 'QUADRATIC ENGINES'
+  suffixes = {
+    'HYPERPROCESSOR',
+    'POWERLIGHT',
+    'POWER CONVERTER',
+    'MOLECULE GENERATOR',
+    'HYPERX',
+    'GENETIC TRANSPORTER',
+    'ACCELERATOR',
+    'QUADRATIC ENGINES',
   }
-  return random_from_list(prefixes)..' '..random_from_list(suffixes)
+  return random_from_list(prefixes) .. ' ' .. random_from_list(suffixes)
 end
 
 function random_verb()
   verbs = {
-    'ACTIVATED', 'STARTED', 'NEUTRALIZED', 'DESTROYED', 'INTERRUPTED',
-    'ACCELERATED', 'HEATING UP', 'SHUTTING DOWN', 'DOWN', 'IGNITED',
-    'TURNED ON', 'BOOTED SUCCESFULLY', 'RECOVERED', 'SENT TO INTERDIMENSIONAL RIFT',
-    'TOOK OFF', 'LANDED'
+    'ACTIVATED',
+    'STARTED',
+    'NEUTRALIZED',
+    'DESTROYED',
+    'INTERRUPTED',
+    'ACCELERATED',
+    'HEATING UP',
+    'SHUTTING DOWN',
+    'DOWN',
+    'IGNITED',
+    'TURNED ON',
+    'BOOTED SUCCESFULLY',
+    'RECOVERED',
+    'SENT TO INTERDIMENSIONAL RIFT',
+    'TOOK OFF',
+    'LANDED',
   }
   return random_from_list(verbs)
 end
@@ -57,17 +100,15 @@ function send_notification()
   else
     phrase = random_build_phrase()
   end
-  phrase = phrase..' '..random_verb()
+  phrase = phrase .. ' ' .. random_verb()
 
-  os.execute('notify-send "'..phrase..'"')
+  os.execute('notify-send "' .. phrase .. '"')
 end
 
 function clicked()
   return os.getenv('BLOCK_BUTTON')
 end
 
-if clicked() then
-  send_notification()
-end
+if clicked() then send_notification() end
 
 print('TURBO')
