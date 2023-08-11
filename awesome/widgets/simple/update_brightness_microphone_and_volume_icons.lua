1ab19b60cebc84174f9e74828ac703ea074278d7
Lucas Perez <lucastperez@gmail.com>
2023-08-11 20:39:18 -0300

    [awesomewm] Add battery update to update_brightness_microphone_and_volume_icons lua script/function

--- awesome/widgets/simple/update_brightness_microphone_and_volume_icons.lua
+++ awesome/widgets/simple/update_brightness_microphone_and_volume_icons.lua
@@ -5,6 +5,10 @@ return {
     require('widgets.simple.volume'):roundUpToNearestEvenNumberOrMultipleOf5()
     require('widgets.simple.microphone'):roundUpToNearestEvenNumberOrMultipleOf5()
     require('widgets.simple.brightness'):roundNearest5()
+    -- I did not want to change the name of the file again
+    -- since it is also used in .profile, but it also updates
+    -- the screen_temperature and battery widgtets.
     require('widgets.simple.screen_temperature'):update_widget()
+    require('widgets.simple.battery'):update_widget()
   end,
 }
