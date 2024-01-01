2e42c9437c85b6993a130734471d0b291576a9cd
Lucas Perez <lucastperez@gmail.com>
2024-01-01 02:22:22 -0300

    [neovim] Remove check of root_dir in gopls, now always sets it
    I did this because when creating a random go file
    in home, I wanted to have LSP, and with that if
    check, the LSP would not start because no root
    directory was found.

--- nvim/lua/plugins/lsp/gopls.lua
+++ nvim/lua/plugins/lsp/gopls.lua
@@ -67,7 +67,8 @@ local function on_attach(client, bufnr)
   })
 
   local root_dir = client.config.root_dir
-  if root_dir then vim.api.nvim_set_current_dir(root_dir) end
+  -- if root_dir then vim.api.nvim_set_current_dir(root_dir) end
+  vim.api.nvim_set_current_dir(root_dir)
 end
 
 return {
