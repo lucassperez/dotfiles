f2c3b765464193a69f3511c700c97a8b2ba95448
Lucas Perez <lucastperez@gmail.com>
2024-01-21 02:45:26 -0300

    [scripts] Add more details to enalbe-touchpad script

--- scripts/enable-touchpad-tap.sh
+++ scripts/enable-touchpad-tap.sh
@@ -4,6 +4,17 @@ echo To be fair, it is probably better to activate this
 echo at the file /usr/share/X11/xorg.conf.d/40-libinput.conf,
 echo using the Option \"Tapping\" \"on\", and while at that,
 echo probably also put Option \"NaturalScrolling\" \"True\".
+echo
+echo Yes, you have to type Option. It should be something like this:
+echo
+echo 'Section "InputClass"'
+echo '        Identifier "libinput touchpad catchall"'
+echo '        MatchIsTouchpad "on"'
+echo '        MatchDevicePath "/dev/input/event*"'
+echo '        Driver "libinput"'
+echo '        Option "Tapping" "on"'
+echo '        Option "NaturalScrolling" "True"'
+echo 'EndSection'
 
 # if [ "$#" = 0 ]; then
 #   touchpad_device_name=$(xinput --list --name-only | grep -i "touchpad" | head -n 1) || exit
