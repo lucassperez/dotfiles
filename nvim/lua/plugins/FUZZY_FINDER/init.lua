3758df2906f46918b8c0feea3baf2fa4341a8eed
Lucas Perez <lucastperez@gmail.com>
2024-02-22 12:16:45 -0300

    [neovim] Fixed a bug where errors in either fuzzy finder would be silently ignored

--- nvim/lua/plugins/FUZZY_FINDER/init.lua
+++ nvim/lua/plugins/FUZZY_FINDER/init.lua
@@ -1,26 +1,21 @@
+local use_telescope = false
+
 local selection = {
-  use_telescope = false,
-  use_fzf_lua = true,
-  default = 'fzf-lua',
+  use_telescope = use_telescope,
+  use_fzf_lua = not use_telescope,
 }
 
+if not selection.use_telescope and not selection.use_fzf_lua then
+  -- If both are false, defaulting to fzf now
+  selection.use_fzf_lua = true
+end
+
 local t
-local ok
 
 if selection.use_fzf_lua then
-  ok, t = pcall(require, 'plugins.FUZZY_FINDER.fzf-lua')
+  t = require('plugins.FUZZY_FINDER.fzf-lua')
 elseif selection.use_telescope then
-  ok, t = pcall(require, 'plugins.FUZZY_FINDER.telescope')
-end
-
-if not ok then
-  P('vamos de default', t)
-  ok, t = pcall(require, 'plugins.FUZZY_FINDER.' .. selection.default)
-end
-
-if not ok then
-  P('ERRO NO FUZZY FINDER: ', selection, t)
-  return ok, t
+  t = require('plugins.FUZZY_FINDER.telescope')
 end
 
 return t
