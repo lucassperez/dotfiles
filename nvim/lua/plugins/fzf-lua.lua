0e5cc663ad96323f2ac0ef3f3db4e4dd21e672e0
Lucas Perez <lucastperez@gmail.com>
2024-02-22 01:25:06 -0300

    [neovim] Swap fuzzy finders config and symlink. Now the real file is in the plugins directory, and the symlink in the FUZZY_FINDER

--- nvim/lua/plugins/fzf-lua.lua
+++ nvim/lua/plugins/fzf-lua.lua
@@ -1,1 +1,120 @@
-/home/lucas/dotfiles/nvim/lua/plugins/FUZZY_FINDER/fzf-lua/config.lua
+local function init()
+  -- FzfLua resume volta a última coisa, interessante hein.
+  -- Dá até pra passar como argumento, tipo FzfLua files resume=true
+
+  -- Can't make git_files show untracked files )=
+  -- Simply passing cmd = 'git ls-files --exclude-standard --cached --others'
+  -- is not working, not even adding git -C to the git root level.
+  -- This should be perfect, but still doesn't work.
+  -- git -C `git rev-parse --show-toplevel` ls-files --exclude-standard --cached --others
+  -- I thought about having a check, if in git repo, use git_files, otherwise,
+  -- use normal files. But since I can't make it show untracked files, I'm
+  -- just using files. By default, it apparently respects .gitignore, but we
+  -- change it by pressing control+g.
+  -- local root = vim.fn.system('git rev-parse --show-toplevel')
+  -- if vim.v.shell_error == 0 then
+  --   require('fzf-lua').git_files({ cwd = root:gsub('\n$', '') })
+  -- else
+  --   require('fzf-lua').files()
+  -- end
+  vim.keymap.set('n', '<C-p>', function()
+    require('fzf-lua').files()
+  end, { desc = '[FzfLua] Abre o FzfLua files' })
+
+  vim.keymap.set('n', '<C-f>', function()
+    -- Not sure the differentece of live_grep and live_grep_native
+    require('fzf-lua').live_grep_native()
+  end, { desc = '[FzfLua] Abre o FzfLua live_grep_native' })
+
+  vim.keymap.set('n', '<leader>p', function()
+    require('fzf-lua').buffers({ sort_lastused = true })
+  end, { desc = '[FzfLua] Abre o FzfLua buffers' })
+
+  vim.keymap.set('n', '<leader>h', function()
+    require('fzf-lua').oldfiles({ sort_lastused = true })
+  end, { desc = '[FzfLua] Abre o FzfLua oldfiles' })
+end
+
+local function setup()
+  require('fzf-lua').setup({
+    keymap = {
+      builtin = {
+        ['<C-d>'] = 'preview-page-down',
+        ['<C-u>'] = 'preview-page-up',
+        ['<C-_>'] = 'toggle-preview',
+
+        -- I did not want to override the defaults, so I added mine
+        -- and then the default present on the readme.
+        -- neovim `:tmap` mappings for the fzf win
+        ['<F1>'] = 'toggle-help',
+        ['<F2>'] = 'toggle-fullscreen',
+        -- Only valid with the 'builtin' previewer
+        ['<F3>'] = 'toggle-preview-wrap',
+        ['<F4>'] = 'toggle-preview',
+        -- Rotate preview clockwise/counter-clockwise
+        ['<F5>'] = 'toggle-preview-ccw',
+        ['<F6>'] = 'toggle-preview-cw',
+        ['<S-down>'] = 'preview-page-down',
+        ['<S-up>'] = 'preview-page-up',
+        ['<S-left>'] = 'preview-page-reset',
+      },
+      fzf = {
+        -- fzf '--bind=' options
+        ['ctrl-z'] = 'abort',
+        ['ctrl-u'] = 'unix-line-discard',
+        ['ctrl-f'] = 'half-page-down',
+        ['ctrl-b'] = 'half-page-up',
+        ['ctrl-a'] = 'beginning-of-line',
+        ['ctrl-e'] = 'end-of-line',
+        ['alt-a'] = 'toggle-all',
+        -- Only valid with fzf previewers (bat/cat/git/etc)
+        ['f3'] = 'toggle-preview-wrap',
+        ['f4'] = 'toggle-preview',
+        ['shift-down'] = 'preview-page-down',
+        ['shift-up'] = 'preview-page-up',
+      },
+    },
+    winopts = {
+      width = 0.95,
+      preview = {
+        -- The percentage that the PREVIEW window takes when opening.
+        -- These numbers do not reflect the preview window size upon rotation,
+        -- only at opening, as far as I could tell.
+        vertical = 'down:45%', -- up|down:size
+        horizontal = 'right:50%', -- right|left:size
+
+        -- Layout horizontal the preview is by the side of the input
+        -- Layout vertical the preview is on top/below of the input
+        -- Flex apparently it tries horizontal,
+        -- but if window is too small, it opens vertical
+        layout = 'flex', -- horizontal|vertical|flex
+
+        delay = 10, -- delay(ms) displaying the preview, prevents lag on fast scrolling
+
+        -- on_create = function()
+        -- called once upon creation of the fzf main window
+        -- can be used to add custom fzf-lua mappings, e.g:
+        --   vim.keymap.set("t", "<C-j>", "<Down>", { silent = true, buffer = true })
+        -- end,
+        -- called once *after* the fzf interface is closed
+        -- on_close = function() ... end
+      },
+    },
+  })
+
+  vim.cmd([[
+  hi clear FzfLuaCursorLine
+  hi link FzfLuaCursorLine Visual
+  hi FzfLuaCursorLine guibg=#61677d gui=NONE
+  hi FzfLuaCursor guifg=NONE guibg=NONE gui=BOLD
+  ]])
+end
+
+return {
+  lazyPluginSpec = {
+    'ibhagwan/fzf-lua',
+    cmd = 'FzfLua',
+    init = init,
+    config = setup,
+  },
+}
--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,1 +1,170 @@
-/home/lucas/dotfiles/nvim/lua/plugins/FUZZY_FINDER/telescope/config.lua
+local function telescope_builtin()
+  return require('telescope.builtin')
+end
+
+local function menufacture()
+  return require('telescope').extensions.menufacture
+end
+
+local function telescopeGitOrFindFiles(opts)
+  opts = opts or {}
+
+  -- https://www.reddit.com/r/neovim/comments/141k38i/telescope_how_to_search_project_directory/
+  local root = vim.fn.system('git rev-parse --show-toplevel')
+  if vim.v.shell_error == 0 then
+    opts.show_untracked = true
+    opts.cwd = opts.cwd or string.gsub(root, '\n', '')
+    menufacture().git_files(opts)
+  else
+    menufacture().find_files(opts)
+  end
+end
+
+local function setup()
+  local telescope = require('telescope')
+  -- local actions = require('telescope.actions')
+
+  telescope.setup({
+    pickers = {
+      lsp_references = { show_line = false },
+      lsp_implementations = { show_line = false },
+      lsp_definitions = { show_line = false },
+      -- diagnostics = { path_display = 'hidden' },
+    },
+    defaults = {
+      layout_config = {
+        vertical = { width = 0.99, height = 0.80 },
+        horizontal = { width = 0.99, height = 0.80 },
+      },
+      mappings = {
+        i = {
+          ['<C-p>'] = 'cycle_history_prev',
+          ['<C-n>'] = 'cycle_history_next',
+
+          ['<C-h>'] = 'which_key',
+
+          ['<C-k>'] = 'move_selection_previous',
+          ['<C-j>'] = 'move_selection_next',
+
+          -- I wanted to have a "faster scrolling", but pag up/down is too fast! :sweat_smile:
+          -- ['<C-p>'] = 'results_scrolling_up',
+          -- ['<C-n>'] = 'results_scrolling_down',
+          -- ['<C-p>'] = actions.move_selection_previous + actions.move_selection_previous + actions.move_selection_previous,
+          -- ['<C-n>'] = actions.move_selection_next + actions.move_selection_next + actions.move_selection_next,
+        },
+        n = {
+          ['m'] = 'toggle_selection',
+        },
+      },
+    },
+    extensions = {
+      fzf = {
+        fuzzy = true, -- false will only do exact matching
+        override_generic_sorter = true, -- override the generic sorter
+        override_file_sorter = true, -- override the file sorter
+        case_mode = 'smart_case', -- or 'ignore_case' or 'respect_case'
+        -- the default case_mode is 'smart_case'
+      },
+      undo = { layout_config = { preview_width = 0.7 } },
+    },
+  })
+
+  -- https://github.com/jchilders/dotfiles/blob/main/xdg_config/nvim/lua/core/highlights.lua
+  -- vim.cmd('autocmd ColorScheme * highlight TelescopeBorder         guifg=#3e4451')
+  -- vim.cmd('autocmd ColorScheme * highlight TelescopePromptBorder   guifg=#3e4451')
+  -- vim.cmd('autocmd ColorScheme * highlight TelescopeResultsBorder  guifg=#3e4451')
+  -- vim.cmd('autocmd ColorScheme * highlight TelescopePreviewBorder  guifg=#525865')
+end
+
+local function keys()
+  return {
+    {
+      mode = 'n',
+      desc = '[Telescope] Abre telescope git ou find files',
+      '<C-p>',
+      function()
+        telescopeGitOrFindFiles()
+      end,
+      { noremap = true },
+    },
+    {
+      mode = 'n',
+      desc = '[Telescope] Abre telescope live grep',
+      '<C-f>',
+      function()
+        -- https://www.reddit.com/r/neovim/comments/141k38i/telescope_how_to_search_project_directory/
+        local root = vim.fn.system('git rev-parse --show-toplevel')
+        if vim.v.shell_error == 0 then
+          menufacture().live_grep({ cwd = string.gsub(root, '\n', '') })
+        else
+          menufacture().live_grep()
+        end
+      end,
+      { noremap = true },
+    },
+    {
+      mode = 'n',
+      desc = '[Telescope] Abre telescope buffers',
+      '<leader>p',
+      function()
+        telescope_builtin().buffers({ sort_mru = true })
+      end,
+      { noremap = true },
+    },
+    {
+      mode = 'n',
+      desc = '[Telescope] Abre telescope old files',
+      '<leader>h',
+      function()
+        telescope_builtin().oldfiles()
+      end,
+      { noremap = true },
+    },
+    {
+      mode = 'n',
+      desc = '[Telescope] Abre telescope find files no caminho de config do nvim',
+      '<leader>zv',
+      function()
+        menufacture().find_files({ cwd = vim.fn.stdpath('config'), show_untracked = true })
+      end,
+      { noremap = true },
+    },
+  }
+end
+
+return {
+  lazyPluginSpec = {
+    'nvim-telescope/telescope.nvim',
+    cmd = 'Telescope',
+    keys = keys(),
+    config = function()
+      setup()
+      -- Setting this here instead of the project_nvim plugin spec because
+      -- I don't want to load telescope when loading project_nvim, since
+      -- I want to lazy load telescope.
+      require('telescope').load_extension('projects')
+    end,
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      {
+        'nvim-telescope/telescope-fzf-native.nvim',
+        build = 'make',
+        config = function()
+          require('telescope').load_extension('fzf')
+        end,
+      },
+      {
+        'debugloop/telescope-undo.nvim',
+        config = function()
+          require('telescope').load_extension('undo')
+        end,
+      },
+      {
+        'molecule-man/telescope-menufacture',
+        config = function()
+          require('telescope').load_extension('menufacture')
+        end,
+      },
+    },
+  },
+}
