94cf54894c029b8c5195f3cafc43f8d88e4ee637
Lucas Perez <lucastperez@gmail.com>
2024-04-03 02:16:37 -0300

    [neovim] Tweak nvim-tree mappings

--- nvim/lua/plugins/nvim-tree/on-attach.lua
+++ nvim/lua/plugins/nvim-tree/on-attach.lua
@@ -66,8 +66,8 @@ local function on_attach(bufnr)
   vim.keymap.set('n', 'J', api.node.navigate.sibling.last, opts('Last Sibling'))
   vim.keymap.set('n', 'K', api.node.navigate.sibling.first, opts('First Sibling'))
   vim.keymap.set('n', 'm', api.marks.toggle, opts('Toggle Bookmark'))
-  vim.keymap.set('n', 'o', api.node.open.edit, opts('Open'))
-  vim.keymap.set('n', 'O', api.node.open.no_window_picker, opts('Open: No Window Picker'))
+  -- vim.keymap.set('n', 'o', api.node.open.edit, opts('Open'))
+  -- vim.keymap.set('n', 'O', api.node.open.no_window_picker, opts('Open: No Window Picker'))
   vim.keymap.set('n', 'p', api.fs.paste, opts('Paste'))
   vim.keymap.set('n', 'P', api.node.navigate.parent, opts('Parent Directory'))
   vim.keymap.set('n', 'q', api.tree.close, opts('Close'))
@@ -88,6 +88,9 @@ local function on_attach(bufnr)
   -- You will need to insert "your code goes here" for any mappings with a custom action_cb
   vim.keymap.set('n', 'l', api.node.open.edit, opts('Open'))
   vim.keymap.set('n', 'h', api.node.navigate.parent_close, opts('Close Directory'))
+  vim.keymap.set('n', 'L', api.node.open.no_window_picker, opts('Open: No Window Picker'))
+  vim.keymap.set('n', 'o', api.fs.create, opts('Create'))
+  vim.keymap.set('n', 'O', api.fs.create, opts('Create'))
 end
 
 return on_attach
