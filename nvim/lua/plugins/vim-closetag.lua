18b2e278086fcc5378920599813ba113e72a661b
Lucas Perez <lucastperez@gmail.com>
2024-12-16 22:38:49 -0300

    [neovim] Add heex to vim-closetag filetypes

--- nvim/lua/plugins/vim-closetag.lua
+++ nvim/lua/plugins/vim-closetag.lua
@@ -1,4 +1,4 @@
-vim.g.closetag_filetypes = 'html,eelixir,elixir,eruby,javascript,javascriptreact,typescript,typescriptreact,template,vue'
+vim.g.closetag_filetypes = 'html,eelixir,elixir,heex,eruby,javascript,javascriptreact,typescript,typescriptreact,template,vue'
 vim.g.closetag_emptyTags_caseSensitive = 1
 vim.g.closetag_regions = {
   ['typescript.tsx'] = 'jsxRegion,tsxRegion',
