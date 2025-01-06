4c116c15d8d2806b628b8193e7f8ed8d30629e2d
Lucas Perez <lucastperez@gmail.com>
2025-01-06 13:34:37 -0300

    [neovim] Add lexima rule when pressing enter between > and <

--- nvim/lua/plugins/lexima.lua
+++ nvim/lua/plugins/lexima.lua
@@ -2,4 +2,7 @@ vim.cmd([[
 let g:lexima_enable_basic_rules   = 0
 let g:lexima_enable_newline_rules = 1
 let g:lexima_enable_endwise_rules = 1
+
+" call lexima#add_rule({'char': '>', 'at': '>\%#<', 'input': "<CR>\t", 'input_after': '<CR>', 'mode': 'i'})
+call lexima#add_rule({'char': '<CR>', 'at': '>\%#<', 'input': "<CR>\t", 'input_after': '<CR>', 'mode': 'i'})
 ]])
