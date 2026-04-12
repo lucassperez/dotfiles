ef261c9b64552c560f1983629eb54deb9729a445
Lucas Perez <lucastperez@gmail.com>
2026-04-11 21:35:18 -0300

    [neovim] Use async option with FzfLua's diagnostics_workspace, lsp_definitions and lsp_implementations

--- nvim/lua/plugins/FUZZY_FINDER/fzf-lua/init.lua
+++ nvim/lua/plugins/FUZZY_FINDER/fzf-lua/init.lua
@@ -1,14 +1,16 @@
+-- async_or_timeout
+-- Set true for async, set an integer for timeout in mili seconds
 return {
   tag = '[FzfLua]',
   project_picker = require('plugins.FUZZY_FINDER.fzf-lua.project'),
   lazyPluginSpec = require('plugins.FUZZY_FINDER.fzf-lua.config').lazyPluginSpec,
 
   diagnostics = function()
-    require('fzf-lua').diagnostics_workspace({ jump1 = false })
+    require('fzf-lua').diagnostics_workspace({ jump1 = false, async_or_timeout = true })
   end,
 
   lsp_definition = function()
-    require('fzf-lua').lsp_definitions({ jump1 = true })
+    require('fzf-lua').lsp_definitions({ jump1 = true, async_or_timeout = true })
   end,
 
   lsp_references = function()
@@ -16,11 +18,12 @@ return {
     require('fzf-lua').lsp_references({
       jump1 = false,
       ignore_current_line = false,
+      async_or_timeout = true,
     })
   end,
 
   lsp_implementation = function()
-    require('fzf-lua').lsp_implementations({ jump1 = false })
+    require('fzf-lua').lsp_implementations({ jump1 = false, async_or_timeout = true })
   end,
 
   code_action = function()
