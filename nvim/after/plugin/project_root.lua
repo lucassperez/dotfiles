eeef3ee9625d7de2d444c8489325ffe1b03da762
Lucas Perez <lucastperez@gmail.com>
2026-05-02 19:31:39 -0300

    [neovim] Refactor project_root

--- nvim/after/plugin/project_root.lua
+++ nvim/after/plugin/project_root.lua
@@ -13,6 +13,16 @@ local root_names = {
   '.svn',
 }
 
+local function change_dir_if_different(path)
+  -- Changed from vim.fn.chdir to vim.cmd.lcd
+  -- With lcd, we have to check with getcwd(0) instead of just getcwd()
+
+  local current = vim.fn.getcwd(0)
+  if current ~= path then
+    vim.cmd.lcd(path)
+  end
+end
+
 -- Cache to use for speed up (at cost of possibly outdated results)
 local root_cache = {}
 
@@ -22,54 +32,84 @@ end
 
 vim.api.nvim_create_user_command('GetProjectRootCache', GetProjectRootCache, {})
 
+local function escape(path)
+  -- If path includes a dot, the lua pattern will treat it
+  -- as the wilcard. So if path is ~/.config/dir as we try
+  -- to match it with something, it will match with ~/aconfig/dir etc.
+  -- vim.pesc should fix that.
+  return vim.pesc(path)
+end
+
+local function normalize(path)
+  -- fs_realpath deals with symlinks
+  return vim.loop.fs_realpath(path) or path
+end
+
 local function set_root()
-  -- Get directory path to start search from
-  local path = vim.api.nvim_buf_get_name(0)
-  if path == '' then return end
+  local full_path = vim.api.nvim_buf_get_name(0)
+  if full_path == '' then return end
 
-  path = vim.fs.dirname(path)
+  full_path = normalize(full_path)
 
-  -- Except inside .asdf, because when going to
-  -- definition I don't want to set it as root
+  -- Exception when inside .asdf, because when going to
+  -- definition I don't want to set it as root.
   local asdf_dir = os.getenv('ASDF_DIR')
-  if asdf_dir then
-    -- First check if using asdf
-    if string.match(path, asdf_dir) then
-      -- Now quits if it matches
+  if asdf_dir then -- First check if using asdf
+    if string.match(full_path, '^' .. escape(normalize(asdf_dir))) then
       return
     end
   end
 
+  local dir_path = vim.fs.dirname(full_path)
+
+  local cached = root_cache[dir_path]
+  if cached then
+    change_dir_if_different(cached)
+    return cached
+  end
+
+  local config_path = normalize(vim.fn.stdpath('config'))
+  if string.match(full_path, '^'..escape(config_path)) then
+    root_cache[dir_path] = config_path
+    change_dir_if_different(config_path)
+    return config_path
+  end
+
   -- If in git, just use root_dir and ignore root_names table.
-  local git_root = vim.fn.system('git rev-parse --show-toplevel'):gsub('\n$', '')
+  local git_root = vim.fn.system(
+    'git -C ' .. vim.fn.shellescape(dir_path) .. ' rev-parse --show-toplevel'
+  ):gsub('\n$', '')
   if vim.v.shell_error == 0 then
-    root_cache[path] = git_root
-    vim.fn.chdir(git_root)
+    root_cache[dir_path] = git_root
+    change_dir_if_different(git_root)
     return git_root
   end
 
-  -- Try cache and resort to searching upward for root directory
-  local root = root_cache[path]
-  if root == nil then
-    local root_file = vim.fs.find(root_names, {
-      path = path,
-      upward = true,
-      stop = vim.loop.os_homedir(),
-      limit = 1, -- stop on first find
-    })[1]
+  local root
+  local root_file = vim.fs.find(root_names, {
+    path = dir_path,
+    upward = true,
+    stop = vim.loop.os_homedir(),
+    limit = 1, -- stop on first find
+  })[1]
 
+  if root_file then
     root = vim.fs.dirname(root_file)
   end
 
   if root == nil then return 'no roots' end
 
-  root_cache[path] = root
-
-  -- Set current directory
-  vim.fn.chdir(root)
+  root_cache[dir_path] = root
+  change_dir_if_different(root)
 end
 
 local root_augroup = vim.api.nvim_create_augroup('MyAutoRoot', {})
+
+vim.api.nvim_create_autocmd('VimEnter', {
+  group = root_augroup,
+  callback = set_root,
+  once = true,
+})
 
 vim.api.nvim_create_autocmd('BufEnter', {
   group = root_augroup,
@@ -77,28 +117,17 @@ vim.api.nvim_create_autocmd('BufEnter', {
   once = false,
 })
 
--- When opening a file from cmd line, like this: vim /path/to/file,
--- the dir would be /path/to, even with BufEnter auto command.
--- From my tests, the dir would change after the BufEnter event ran,
--- so I'm setting this second autocmd, but to run only once.
--- Also, when vim is ran without a filepath, just "vim", it
--- does not fire this event, meaning that on first DirChanged it would
--- run this. So I'm checking if vim was opened with any arguments
--- using argc, which returns the number of FILES in the argument list.
--- Flags will not be counted, so both
--- `vim -O file1 file2` and `vim file1 file2` returns 2.
-if vim.fn.argc() > 0 then
-  vim.api.nvim_create_autocmd('DirChanged', {
-    group = root_augroup,
-    callback = function()
-      set_root()
-      -- If callback returns true,
-      -- the autocmd is deleted once ran.
-      -- return true
-    end,
-    -- But if the once option is set to true,
-    -- it is also deleted afterwards.
-    -- :h autocmd-once
-    once = true,
-  })
-end
+-- When opening symlinks, sometimes things are weird.
+-- I can't use :BlameToggle when opening a symlink even though
+-- the original file is inside a git directory.
+-- The code below would solve it, but also looks a little extreme.
+-- vim.api.nvim_create_autocmd({ 'BufReadPost', 'BufNewFile' }, {
+--   callback = function()
+--     local old = vim.api.nvim_buf_get_name(0)
+--     local real = vim.loop.fs_realpath(old)
+--     -- If opens a symlink, follows it and rename the buffer
+--     if real and real ~= old then
+--       vim.api.nvim_buf_set_name(0, real)
+--     end
+--   end,
+-- })
