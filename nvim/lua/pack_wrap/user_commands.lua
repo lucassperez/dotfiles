3cccd3ba251f39fb70cfeb2b2388408fab3cd864
Lucas Perez <lucastperez@gmail.com>
2026-05-02 17:09:16 -0300

    [neovim] Create Pack generic user command that defaults to list

--- nvim/lua/pack_wrap/user_commands.lua
+++ nvim/lua/pack_wrap/user_commands.lua
@@ -85,6 +85,44 @@ end
 
 return {
   create = function(pack_wrap_loaded, resolved)
+    vim.api.nvim_create_user_command('Pack', function(opts)
+      local cmd = opts.fargs[1] or 'list'
+      local args = {}
+      for i = 2, #opts.fargs do
+        table.insert(args, opts.fargs[i])
+      end
+
+      if cmd == 'list' then
+        cmd = 'PackList'
+      elseif cmd == 'update' then
+        cmd = 'PackUpdate'
+      elseif cmd == 'delete' then
+        cmd = 'PackDelete'
+      else
+        vim.notify('Invalid command: ' .. cmd, vim.log.levels.ERROR)
+        return
+      end
+
+      vim.api.nvim_cmd({
+        cmd = cmd,
+        bang = opts.bang,
+        args = args
+      }, {})
+    end, {
+      nargs = '*',
+      bang = true,
+      complete = function(_, cmd_line, cursor_pos)
+        local before_cursor = cmd_line:sub(1, cursor_pos)
+        local args = vim.split(before_cursor, '%s+')
+
+        if #args <= 2 then
+          return { 'list', 'update', 'delete' }
+        end
+
+        return complete_resolved(resolved)()
+      end
+    })
+
     vim.api.nvim_create_user_command('PackList', function(opts)
       local buf = open_pack_list_window({ "Loading..." }, opts.bang)
 
@@ -100,7 +138,7 @@ return {
           local format_str = "   %s %03s  %-30s [%s] %-10s"
 
           if pack.spec.version then
-            format_str = format_str .. '\t(%s)'
+            format_str = format_str .. ' (%s)'
           end
 
           local active = pack.active
@@ -115,7 +153,7 @@ return {
             active_loaded_str = '  '
           end
 
-          table.insert(lines, string.format(
+          local line = string.format(
             format_str,
             active_loaded_str,
             i,
@@ -123,7 +161,9 @@ return {
             string.sub(pack.rev, 1, 6),
             pack.branches and pack.branches[1],
             pack.spec.version
-          ))
+          ):gsub('%s+$', '')
+
+          table.insert(lines, line)
         end
 
         vim.bo[buf].modifiable = true
@@ -139,19 +179,21 @@ return {
 
     vim.api.nvim_create_user_command('PackUpdate', function(opts)
       if #opts.fargs > 0 then
-        vim.pack.update(opts.fargs)
+        vim.pack.update(opts.fargs, { force = opts.bang })
       else
-        vim.pack.update()
+        vim.pack.update({ force = opts.bang })
       end
     end, {
       nargs = '*',
+      bang = true,
       complete = complete_resolved(resolved),
     })
 
     vim.api.nvim_create_user_command('PackDelete', function(opts)
-      vim.pack.del(opts.fargs)
+      vim.pack.del(opts.fargs, { force = opts.bang })
     end, {
       nargs = '+',
+      bang = true,
       complete = complete_resolved(resolved),
     })
   end
