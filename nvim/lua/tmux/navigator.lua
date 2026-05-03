ba8d15e719733796d2c3f8f1dc6502ef19a2db06
Lucas Perez <lucastperez@gmail.com>
2026-05-03 02:27:19 -0300

    [neovim,tmux] Dont't remove tmux zoom when changing panes

--- nvim/lua/tmux/navigator.lua
+++ nvim/lua/tmux/navigator.lua
@@ -10,7 +10,8 @@ local function navigate(direction, tmux_direction)
   end
 end
 
-vim.keymap.set('n', '<C-h>', function () navigate('h', '-L') end)
-vim.keymap.set('n', '<C-j>', function () navigate('j', '-D') end)
-vim.keymap.set('n', '<C-k>', function () navigate('k', '-U') end)
-vim.keymap.set('n', '<C-l>', function () navigate('l', '-R') end)
+-- The -Z flag tells tmux to keep zoom state after select-pane.
+vim.keymap.set('n', '<C-h>', function () navigate('h', '-Z -L') end)
+vim.keymap.set('n', '<C-j>', function () navigate('j', '-Z -D') end)
+vim.keymap.set('n', '<C-k>', function () navigate('k', '-Z -U') end)
+vim.keymap.set('n', '<C-l>', function () navigate('l', '-Z -R') end)
