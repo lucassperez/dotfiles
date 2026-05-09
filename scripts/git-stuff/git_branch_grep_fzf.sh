b64594bbe0688ab6438e0453106a9f930fe23738
Lucas Perez <lucastperez@gmail.com>
2026-05-09 17:05:59 -0300

    [scripts] Change echo for printf in git_branch_grep_fzf.sh script

--- scripts/git-stuff/git_branch_grep_fzf.sh
+++ scripts/git-stuff/git_branch_grep_fzf.sh
@@ -1,20 +1,19 @@
 #!/bin/sh
 
 imprimir_mensagem_mudando_de_branch() {
-  echo "Mudando para \e[1m$1\e[0m"
+  printf "Mudando para \e[1m$1\e[0m\n"
 }
 
 if [ "$#" -eq 0 ]; then
   branches="`git branch`"
-  count=`echo "$branches" | wc -l | tr -d '[[:blank:]]'`
-  selected=`echo "$branches" | fzf --height=$(expr 2 + $count)`
-  echo $selected
+  count=`printf "$branches\n" | wc -l | tr -d '[[:blank:]]'`
+  selected=`printf "$branches" | fzf --height=$(expr 2 + $count)`
 
   if [ -z "$selected" ]; then
     exit 3
   fi
 
-  selected=`echo "$selected" | sed -E 's/^[*+]?[[:blank:]]*//'`
+  selected=`printf "$selected" | sed -E 's/^[*+]?[[:blank:]]*//'`
   imprimir_mensagem_mudando_de_branch "$selected"
   git checkout "$selected"
   exit
@@ -33,19 +32,19 @@ while [ "$#" -gt 0 ]; do
       shift
       ;;
     -h|--help)
-      echo 'Basicamente, recebe um padrão e busca uma branch git que casa com esse'
-      echo 'padrão. Caso exista mais de uma, joga as possíveis branches no fzf.'
-      echo 'A busca com o padrão ignora maiúsculas e minúsculas (grep -i).'
-      echo 'Caso nenhum padrão seja passado, passa todas as branches direto para o fzf.'
-      echo 'Caso duas branches casem com o padrão mas uma delas for a branch atual,'
-      echo 'mudamos direto para a outra branch.'
-      echo
-      echo 'Também é possível passar algumas opções para alterar a funcionalidade:'
-      echo
-      echo '-           Ir para a última branch (git checkout -)'
-      echo '.           Executa git checkout .'
-      echo '-b          Criar e mudar para a branch (git checkout -b)'
-      echo '-h --help   Mostra esse texto de ajuda'
+      printf 'Basicamente, recebe um padrão e busca uma branch git que casa com esse\n'
+      printf 'padrão. Caso exista mais de uma, joga as possíveis branches no fzf.\n'
+      printf 'A busca com o padrão ignora maiúsculas e minúsculas (grep -i).\n'
+      printf 'Caso nenhum padrão seja passado, passa todas as branches direto para o fzf.\n'
+      printf 'Caso duas branches casem com o padrão mas uma delas for a branch atual,\n'
+      printf 'mudamos direto para a outra branch.'
+      printf
+      printf 'Também é possível passar algumas opções para alterar a funcionalidade:\n'
+      printf
+      printf '-           Ir para a última branch (git checkout -)\n'
+      printf '.           Executa git checkout .\n'
+      printf '-b          Criar e mudar para a branch (git checkout -b)\n'
+      printf '-h --help   Mostra esse texto de ajuda\n'
       exit
       ;;
     *)
@@ -57,19 +56,19 @@ while [ "$#" -gt 0 ]; do
 done
 
 if [ -n "$checkout_last" ]; then
-  echo Mudando para a última branch
+  printf 'Mudando para a última branch\n'
   git checkout -
   exit
 fi
 
 if [ -n "$create_and_checkout" ]; then
-  echo "Criando e mudando para \e[1m$create_and_checkout\e[0m"
+  printf "Criando e mudando para \e[1m$create_and_checkout\e[0m\n"
   git checkout -b "$create_and_checkout"
   exit
 fi
 
 if [ -n "$checkout_dot" ]; then
-  echo 'Executando `git checkout .`'
+  printf 'Executando `git checkout .`\n'
   git checkout .
   exit
 fi
@@ -78,32 +77,32 @@ fi
 partial=`git branch | grep -i $grep_pattern`
 
 if [ -z "$partial" ]; then
-  echo Nenhuma branch encontrada para o grep com "$grep_pattern"
+  printf "Nenhuma branch encontrada para o grep com $grep_pattern\n"
   exit 10
 fi
 
-count=`echo "$partial" | wc -l`
-if [ "$count" -eq 2 ] && echo "$partial" | grep -q '^*'; then
+count=`printf "$partial" | wc -l`
+if [ "$count" -eq 2 ] && printf "$partial" | grep -q '^*'; then
   # Esse sed dá match na branch que não é a atual
   # e tira os espaços do começo que houverem.
-  b=`echo "$partial" | sed -n 's/^[^*]\s*//p'`
-  echo "Duas branches encontradas, mas uma era a atual.\nTrocando para a outra: \e[1m$b\e[0m"
-  git checkout `echo "$partial" | grep -v '^*'`
+  b=`printf "$partial" | sed -n 's/^[^*]\s*//p'`
+  printf "Duas branches encontradas, mas uma era a atual.\nTrocando para a outra: \e[1m$b\e[0m\n"
+  git checkout `printf "$partial" | grep -v '^*'`
   exit
 elif [ "$count" -gt 1 ]; then
-  echo Resultado com mais de uma branch encontrada para o grep com "$grep_pattern"
-  selected=`echo "$partial" | fzf --height=$(expr 2 + $count)`
+  printf "Resultado com mais de uma branch encontrada para o grep com $grep_pattern\n"
+  selected=`printf "$partial" | fzf --height=$(expr 2 + $count)`
 
   if [ -z "$selected" ]; then
     exit 11
   fi
 
-  selected=`echo "$selected" | tr -d '*' | tr -d '[[:blank:]]'`
+  selected=`printf "$selected" | tr -d '*' | tr -d '[[:blank:]]'`
   imprimir_mensagem_mudando_de_branch "$selected"
-  git checkout `echo $selected | sed 's/^\*\?\s*//'`
+  git checkout `printf $selected | sed 's/^\*\?\s*//'`
   exit
 fi
 
-b=`echo "$partial" | tr -d '*' | tr -d '[[:blank:]]'`
+b=`printf "$partial" | tr -d '*' | tr -d '[[:blank:]]'`
 imprimir_mensagem_mudando_de_branch "$b"
 git checkout "$b"
