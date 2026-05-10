333c55825060aed7a71c708fa668f501bee1d9e7
Lucas Perez <lucastperez@gmail.com>
2026-05-09 21:11:54 -0300

    [neovim] Add custom textobjects for ignore_blank_line true and false in mini.comment

--- nvim/lua/plugins/mini/comment.lua
+++ nvim/lua/plugins/mini/comment.lua
@@ -1,12 +1,9 @@
 require('mini.comment').setup({
   mappings = {
-    textobject = 'u',
+    textobject = '',
   },
   options = {
     ignore_blank_line = true,
-
-    -- My custom option used in the monkey patch below.
-    ignore_blank_line_textobject = false,
 
     custom_commentstring = function()
       return require('ts_context_commentstring').calculate_commentstring() or vim.bo.commentstring
@@ -14,21 +11,14 @@ require('mini.comment').setup({
   },
 })
 
-local original_textobject = MiniComment.textobject
--- I know it is duplicated, I'm monkey patching it.
----@diagnostic disable-next-line: duplicate-set-field
-MiniComment.textobject = function()
-  local ibl_og = MiniComment.config.options.ignore_blank_line
-  local ibl_to = MiniComment.config.options.ignore_blank_line_textobject
+vim.keymap.set('o', 'u', function()
+  vim.b.minicomment_config = { options = { ignore_blank_line = false } }
+  MiniComment.textobject()
+  vim.b.minicomment_config = nil
+end)
 
-  if ibl_to ~= nil and ibl_og ~= ibl_to then
-    MiniComment.config.options.ignore_blank_line = ibl_to
-    local ok, err = pcall(original_textobject)
-    MiniComment.config.options.ignore_blank_line = ibl_og
-    if not ok then
-      error(err)
-    end
-  else
-    original_textobject()
-  end
-end
+vim.keymap.set('o', 'U', function()
+  vim.b.minicomment_config = { options = { ignore_blank_line = true } }
+  MiniComment.textobject()
+  vim.b.minicomment_config = nil
+end)
