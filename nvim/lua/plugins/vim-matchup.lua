3ddb2d57cdb1346b9fc0421d8f254d25fb41b4d7
Lucas Perez <lucastperez@gmail.com>
2026-05-26 14:59:31 -0300

    [neovim] Disable vim-matchup on ruby rspec files with 2500 lines

--- nvim/lua/plugins/vim-matchup.lua
+++ nvim/lua/plugins/vim-matchup.lua
@@ -5,13 +5,21 @@ vim.g.matchup_matchparen_offscreen = {}
 vim.api.nvim_create_autocmd({ 'BufReadPost' }, {
   callback = function(args)
     local max_lines = 5000
+    local bufnr = args.buf
+
+    if
+      vim.bo[bufnr].filetype == 'ruby'
+      and vim.api.nvim_buf_get_name(bufnr):match('_spec%.rb$')
+    then
+      max_lines = 2500
+    end
 
     local ok, line_count = pcall(vim.api.nvim_buf_line_count, args.buf)
     if not ok then return end
 
     if line_count > max_lines then
       -- Disable only for current buffer
-      vim.b[args.buf].matchup_matchparen_enabled = 0
+      vim.b[bufnr].matchup_matchparen_enabled = 0
     end
   end,
 })
