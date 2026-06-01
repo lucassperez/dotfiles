b2fa568dbace64ca8760dec6d4e2f790a7b3a8dd
Lucas Perez <lucastperez@gmail.com>
2026-06-01 17:18:47 -0300

    [neovim] Fix tabline BDelete and BWipeout using invalid buffers for replacement, leaving state.buffers empty sometimes, maybe. Maybe not. Hopefully this fixes it.
    When closing current buffer, sometimes the tabline
    could choose an unlisted buffer to replace the
    current one.
    I BELIEVE this was causing the state.buffers to be
    empty sometimes, effectly making the tabline show
    no buffers, despite having one opened right in
    front of us. Putting the unlisted buffer probably
    left the pending_delete stale.
    But maybe not, I don't know.
    Hopefully this fixes the empty buffers. But it
    really was wrong, so I guess it is a win.

--- nvim/lua/tabline/user_commands/buf_delete.lua
+++ nvim/lua/tabline/user_commands/buf_delete.lua
@@ -1,13 +1,35 @@
 return function(state)
+  local function recur_find_next(i, direction)
+    if i < 1 or i > #state.buffers then
+      return nil
+    end
+
+    local candidate_bufnr = state.buffers[i].bufnr
+    --[[
+    With this logic, if for example we have these buffers:
+      A, Netrw, B
+    If we are at B and call the BDelete command, we will
+    land in A, and not in Netrw buffer.
+    Same goes for Blame, NvimTree, etc. And any other non buflisted buffer.
+    Basically the "special" buffers.
+    I didn't mind skipping the unliested buffer, in fact I preferred doing so,
+    at list with Netrw/NvimTree, but this is something to think about.
+    Maybe unlisted but not floating windows should not be skipped.
+    Maybe the options about unlisted buffers could decide this behaviour.
+    Maybe a specific option to control what the user wants could be added.
+    So one day I could come revisit this.
+    ]]
+    if vim.api.nvim_buf_is_valid(candidate_bufnr) and vim.bo[candidate_bufnr].buflisted then
+      return candidate_bufnr
+    end
+
+    return recur_find_next(i + direction, direction)
+  end
+
   local function find_replacement(bufnr)
     for i, buf in ipairs(state.buffers) do
       if buf.bufnr == bufnr then
-        if i < #state.buffers then
-          return state.buffers[i + 1].bufnr
-        elseif i > 1 then
-          return state.buffers[i - 1].bufnr
-        end
-        return nil
+        return recur_find_next(i + 1, 1) or recur_find_next(i - 1, -1)
       end
     end
     return nil
@@ -23,25 +45,34 @@ return function(state)
       return
     end
 
-    local replacement = nil
-
     state.pending_delete[bufnr] = true
 
-    for _, win in ipairs(vim.api.nvim_list_wins()) do
-      if vim.api.nvim_win_get_buf(win) == bufnr then
-        replacement = replacement or find_replacement(bufnr)
+    local replacement = nil
+    local ok, err = pcall(function()
+      for _, win in ipairs(vim.api.nvim_list_wins()) do
+        if vim.api.nvim_win_get_buf(win) == bufnr then
+          if not replacement then
+            replacement = find_replacement(bufnr) or vim.api.nvim_create_buf(true, false)
+          end
 
-        if replacement then
-          vim.api.nvim_win_set_buf(win, replacement)
-        else
-          replacement = vim.api.nvim_create_buf(true, false)
           vim.api.nvim_win_set_buf(win, replacement)
         end
       end
+    end)
+
+    if not ok then
+      vim.notify(
+        string.format(
+          'tabline#%s (window swap): %s'
+          .. "\nProceeding to try and delete/wipe the buffer anyways to honor the user's command",
+          cmd_name, vim.inspect(err)
+        ),
+        vim.log.levels.WARN
+      )
     end
 
     if vim.api.nvim_buf_is_valid(bufnr) then
-      local ok, err = pcall(vim.api.nvim_cmd, { cmd = vim_cmd, args = { bufnr }, bang = force }, {})
+      ok, err = pcall(vim.api.nvim_cmd, { cmd = vim_cmd, args = { bufnr }, bang = force }, {})
 
       if not ok then
         vim.notify('tabline#' .. cmd_name .. ': ' .. vim.inspect(err), vim.log.levels.ERROR)
